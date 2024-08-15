de938ea03795baeaac8be4a770ef9823f0a3b029
Ualzem2024 <novogithub@gmail.com>
2024-08-15 17:54:04 -0300

    Blog Categories

---
+++ wp-content/themes/ualzem-2024/author.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+
+
+<main class="container page section">
+
+    <!--  EXIBINDO NOME E DESCRIÇÃO DO AUTOR:   -->
+    <?php
+    $author = get_queried_object();
+    ?>
+    <h2 class="text-center primary-text">
+        Autor: <?php echo $author->data->display_name; ?>
+    </h2>
+    <p class="text-center"><?php echo get_the_author_meta('description', $author->data->ID); ?></p>
+
+    <?php get_template_part('template-parts/blog', 'loop'); ?>
+
+
+
+
+</main>
+
+
+<?php get_footer(); ?>
